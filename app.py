2ac4c9ccd3c07129faa35f2c863408a86331ebee
Sachith <110117965+sachith-d@users.noreply.github.com>
2023-03-30 14:17:54 +0530

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from app.generator import generateImg
+from app.main import generateImg
 
 app = Flask(__name__)
 app.secret_key = "key"  
