aa5a67f14814dea8bd2814119d865ffacdd56d89
Sachith <110117965+sachith-d@users.noreply.github.com>
2023-03-31 12:31:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,15 +2,17 @@ import os
 import openai
 from dotenv import load_dotenv
 
+# Loading environment variables from a .env file
 load_dotenv()
 
 def generateImg(text, sz):
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
+    openai.api_key = os.getenv("OPENAI_API_KEY") # Setting the API key for OpenAI
+    
+    # Creating an image using given prompt text and image size
     img = openai.Image.create(
         prompt = text,
         n = 1,
         size = sz
     )
 
-    return img["data"][0]["url"]
+    return img["data"][0]["url"] # Returning the URL of the generated image
